b9d570499b584c51c1ff5d98279ff05745d7855c
Bartosz Krajka <krajka.bartosz@gmail.com>
2017-08-24 21:06:45 +0200

    Implement Invoice validation - WIP

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,5 +1,14 @@
 class InvoicesController < ApplicationController
   def create
+    Invoice.create(invoice_params)
     head :created
+  rescue ActionController::ParameterMissing
+    head :bad_request
+  end
+
+  private
+
+  def invoice_params
+    params.require(:invoice).permit(:number)
   end
 end
--- spec/requests/invoice_spec.rb
+++ spec/requests/invoice_spec.rb
@@ -2,9 +2,24 @@ require "rails_helper"
 
 RSpec.describe "Invoice requests", :type => :request do
   describe 'POST /invoice' do
-    it 'returns 201' do
-      post '/invoice'
-      expect(response).to have_http_status(:created)
+    let(:params) {
+      { invoice: { number: 'abc' } }
+    }
+
+    context 'happy path' do
+      it 'returns 201' do
+        post '/invoice', params: params
+        expect(response).to have_http_status(:created)
+      end
+    end
+
+    context 'missing number' do
+      it 'returns 400' do
+        invalid_params = params
+        invalid_params[:invoice].delete(:number)
+        post '/invoice', params: invalid_params
+        expect(response).to have_http_status(:bad_request)
+      end
     end
   end
 end
